938c93fbb848cd3aa21247423eb3c660f06f9462
demaro <demaromail@gmail.com>
2017-03-12 13:53:36 -0300

    actuliza

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -43,7 +43,7 @@ def index(request):
 
 @login_required
 @cache_page(6000)
-def home(request, **kwargs):
+def home(request, id_especilidad):
             #Urologia
       total_art      = Pedido.objects.filter(especialidad=1).count()
       pend           = Pedido.objects.filter(especialidad=1).filter(estado='pendiente').count()
@@ -116,7 +116,7 @@ def home(request, **kwargs):
 
       encargado      = Encargado.objects.all()
       especialidad  = Especialidad.objects.all()
-      pedido  = Pedido.objects.all()
+      pedido  = Pedido.objects.filter(especialidad=id_especilidad)
       template = "index3.html"
       return render_to_response(template,locals())
 
