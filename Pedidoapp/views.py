82392b9cda41d7fe7bbae8b17ba528b37a7688ac
demaro <demaromail@gmail.com>
2017-03-11 23:26:17 -0300

    actuliza

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -122,14 +122,14 @@ def home(request):
 
 
 @cache_page(6000)
-def ArticuloListView(request, id_especialidad):
+def ArticuloListView(request):
     user = request.user
     if user.is_superuser:
         pedido = Pedido.objects.all()
         template  = 'admindata.html'
         return render_to_response(template,locals())
     else:
-        pedido = Pedido.objects.filter(especialidad=id_especialidad)
+        pedido = Pedido.objects.filter(especialidad=1)
     template  = 'index2.html'
     return render_to_response(template,locals())
 
