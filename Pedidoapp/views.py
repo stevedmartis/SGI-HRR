be6a9089a5521fcfb1d05df7a237bb020ebb583d
demaro <demaromail@gmail.com>
2017-03-12 17:53:35 -0300

    actualizado

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -116,8 +116,7 @@ def home(request):
 
       encargado      = Encargado.objects.all()
       especialidad  = Especialidad.objects.all().order_by('nombre')
-      contexto = {'especialidad':especialidad}
-      return render(request, 'index3.html', contexto)
+      return render(request, 'index2.html')
 
 @cache_page(6000)
 def ArticuloListView(request):
