8c6e3bd49eeedd0f3461e7dd27bf59ea13fc8fd5
demaro <demaromail@gmail.com>
2017-03-13 22:53:05 -0300

    dale

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -230,9 +230,10 @@ def Pedido_Edit(request, id_pedido):
       return custom_redirect('usuario:lita_todo')
     return render(request, 'form.html', {'form':form})
 
-    def custom_redirect(lita_todo, *args, **kwargs):
     from django.core.urlresolvers import reverse 
     import urllib
+
+    def custom_redirect(lita_todo, *args, **kwargs):
     url = reverse(lita_todo, args = args)
     params = urllib.urlencode(kwargs)
     return HttpResponseRedirect('/solicitar/lista/' + "(?P<id_especialidad>\d+)" % params)
