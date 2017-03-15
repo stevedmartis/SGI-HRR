627651238ceae51e1dfd39f19fe47184f4e9e88b
demaro <demaromail@gmail.com>
2017-03-15 18:41:04 -0300

    dale

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -216,7 +216,6 @@ def ListAll(request, id_especialidad):
       if form.is_valid():
           form.save()
       return HttpResponseRedirect('/solicitar/lista/%s/' % id_especialidad)
-    if request.method == 'GET':
   user = request.user
   if user.is_superuser:
       template = 'admindata.html'
