527fe232e8902c2b22cef40eafba2cad18f9ecc5
demaro <demaromail@gmail.com>
2017-03-15 20:15:28 -0300

    dale

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -210,12 +210,12 @@ def home(request):
 def ListAll(request, id_especialidad):
   especialidad = Especialidad.objects.get(id=id_especialidad)
   pedido = Pedido.objects.filter(especialidad=especialidad)
-    user = request.user
-    if user.is_superuser:
+  user = request.user
+  if user.is_superuser:
         template = 'admindata.html'
-    return render(request, template, {'pedido':pedido, 'especialidad':especialidad})
-    else:
-    return render(request, 'index2.html', {'pedido':pedido, 'especialidad':especialidad})
+  return render(request, template, {'pedido':pedido, 'especialidad':especialidad})
+  else:
+  return render(request, 'index2.html', {'pedido':pedido, 'especialidad':especialidad})
 
 @login_required
 def Cant_ingresar(request, id_pedido, id_especialidad):
