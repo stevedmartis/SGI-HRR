8140e540478046bbed0d0516b95eb9292c6740b8
demaro <demaromail@gmail.com>
2017-03-16 10:26:47 -0300

    dale

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -247,6 +247,7 @@ def Cant_ingresar(request, id_pedido, id_especialidad):
 def Cant_update(request, id_pedido, id_especialidad):
     especialidad = Especialidad.objects.get(id=id_especialidad)
     pedido = Pedido.objects.get(id=id_pedido)
+    pedido1 = Pedido.objects.filter(articulo=pedido)
     if request.method == 'GET':
       form = PedidoEditForm(instance=pedido)
     else:
@@ -256,7 +257,7 @@ def Cant_update(request, id_pedido, id_especialidad):
           pedido.estado = 'modificado'
           pedido.save()
       return HttpResponseRedirect('/solicitar/lista_super/%s/' % id_especialidad)
-    return render(request, 'form.html', {'form':form})
+    return render(request, 'form.html', {'form':form, 'pedido1':pedido1})
 
 
 
