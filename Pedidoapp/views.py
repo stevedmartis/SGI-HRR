6d93e89f2ea88aab5e33247059706fbf4fd48c46
demaro <demaromail@gmail.com>
2017-03-16 09:50:44 -0300

    dale

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -233,6 +233,7 @@ def ListEspeci(request, id_especialidad):
 def Cant_ingresar(request, id_pedido, id_especialidad):
     especialidad = Especialidad.objects.get(id=id_especialidad)
     pedido = Pedido.objects.get(id=id_pedido)
+    nombre = Pedido.objects.filter(articulo=pedido)
     if request.method == 'GET':
       form = PedidoEditForm(instance=pedido)
     else:
@@ -248,6 +249,7 @@ def Cant_ingresar(request, id_pedido, id_especialidad):
 def Cant_update(request, id_pedido, id_especialidad):
     especialidad = Especialidad.objects.get(id=id_especialidad)
     pedido = Pedido.objects.get(id=id_pedido)
+    nombre = Pedido.objects.filter(articulo=pedido)
     if request.method == 'GET':
       form = PedidoEditForm(instance=pedido)
     else:
