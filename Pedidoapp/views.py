5b05c6ac3c728c274d170faadc2a767f78fc016e
demaro <demaromail@gmail.com>
2017-03-20 22:45:16 -0300

    pdf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*- 
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
