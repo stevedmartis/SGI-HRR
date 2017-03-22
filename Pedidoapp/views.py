2d3f5848bca8e77281e74704c96badca8a423868
demaro <demaromail@gmail.com>
2017-03-22 10:29:59 -0300

    utf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -368,7 +368,7 @@ def Completar(request, id_especialidad):
     especialidad.estado = "completado"
     especialidad.save()
     return HttpResponseRedirect('/solicitar/home/')
-
+"""
 from reportlab.pdfgen import canvas
 from io import BytesIO
 from django.views.generic import View
@@ -379,6 +379,7 @@ from reportlab.lib.pagesizes import letter,A4,A5, A3, A2
 from reportlab.platypus import (BaseDocTemplate, Frame, Paragraph, NextPageTemplate, PageBreak, PageTemplate)
 from reportlab.platypus.tables import Table, TableStyle
 import datetime
+"""
 mylist = []
 today = datetime.date.today()
 mylist.append(today)
@@ -445,133 +446,81 @@ class ReportePedidosPDF(View):
         return response
 
 
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle, TA_CENTER
-from reportlab.lib.units import inch, mm
-from reportlab.pdfgen import canvas
-from reportlab.platypus import Paragraph, Table, SimpleDocTemplate, Spacer
- 
-########################################################################
-class Test(object):
-    """"""
- 
-    #----------------------------------------------------------------------
-    def __init__(self):
-        """Constructor"""
-        self.width, self.height = letter
-        self.styles = getSampleStyleSheet()
- 
-    #----------------------------------------------------------------------
-    def coord(self, x, y, unit=1):
-        """
-        http://stackoverflow.com/questions/4726011/wrap-text-in-a-table-reportlab
-        Helper class to help position flowables in Canvas objects
-        """
-        x, y = x * unit, self.height -  y * unit
-        return x, y
- 
-    #----------------------------------------------------------------------
-    def run(self):
-        """
-        Run the report
-        """
-        self.doc = SimpleDocTemplate("test.pdf")
-        self.story = [Spacer(1, 2.5*inch)]
-        self.createLineItems()
- 
-        self.doc.build(self.story, onFirstPage=self.createDocument)
-        print "finished!"
- 
-    #----------------------------------------------------------------------
-    def createDocument(self, canvas, doc):
-        """
-        Create the document
-        """
-        self.c = canvas
-        normal = self.styles["Normal"]
- 
-        header_text = "<b>This is a test header</b>"
-        p = Paragraph(header_text, normal)
-        p.wrapOn(self.c, self.width, self.height)
-        p.drawOn(self.c, *self.coord(100, 12, mm))
- 
-        ptext = """Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-        culpa qui officia deserunt mollit anim id est laborum."""
- 
-        p = Paragraph(ptext, style=normal)
-        p.wrapOn(self.c, self.width-50, self.height)
-        p.drawOn(self.c, 30, 700)
- 
-        ptext = """
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui 
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores 
-        et quas molestias excepturi sint occaecati cupiditate non provident, 
-        similique sunt in culpa qui officia deserunt mollitia animi, id est laborum
-        et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
-        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
-        quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est,
-        omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut 
-        rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et 
-        molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-        ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat.
-        """
-        p = Paragraph(ptext, style=normal)
-        p.wrapOn(self.c, self.width-50, self.height)
-        p.drawOn(self.c, 30, 600)
- 
-    #----------------------------------------------------------------------
-    def createLineItems(self):
-        """
-        Create the line items
-        """
-        text_data = ["Line", "DOS", "Procedure<br/>/Modifier",
-                     "Description", "Units", "Billed<br/>Charges",
-                     "Type1<br/>Reductions", "Type2<br/>Reductions",
-                     "Type3<br/>Reductions", "Allowance",
-                     "Qualify<br/>Code"]
-        d = []
-        font_size = 8
-        centered = ParagraphStyle(name="centered", alignment=TA_CENTER)
-        for text in text_data:
-            ptext = "<font size=%s><b>%s</b></font>" % (font_size, text)
-            p = Paragraph(ptext, centered)
-            d.append(p)
- 
-        data = [d]
- 
-        line_num = 1
- 
-        formatted_line_data = []
- 
-        for x in range(200):
-            line_data = [str(line_num), "04/12/2013", "73090", 
-                         "Test Reflexes", "1", "131.00", "0.00", 
-                         "0.00", "0.00", "0.00", "1234"]
- 
-            for item in line_data:
-                ptext = "<font size=%s>%s</font>" % (font_size-1, item)
-                p = Paragraph(ptext, centered)
-                formatted_line_data.append(p)
-            data.append(formatted_line_data)
-            formatted_line_data = []
-            line_num += 1
- 
-        table = Table(data, colWidths=[20, 40, 45, 120, 30, 40, 
-                                       50, 50, 50, 50, 30])
- 
-        self.story.append(table)
- 
-#----------------------------------------------------------------------
-if __name__ == "__main__":
-    t = Test()
-    t.run()
+#Librerias reportlab a usar:
+from reportlab.platypus import (BaseDocTemplate, Frame, Paragraph, 
+                    NextPageTemplate, PageBreak, PageTemplate)
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import A4
 
+
+#NIVEL 1: CREAMOS LOS CANVAS
+#===========================   
+#Creamos los canvas para el pie de página y encabezado, que serán fijos
+class Test(View): 
+def encabezado(canvas,doc):
+    canvas.saveState()
+    canvas.setFont('Times-Roman',9)
+    canvas.drawString(inch, A4[1]-50, "Ejemplo de DocTemplate y PageTemplate")
+    canvas.restoreState()
+    
+def pie(canvas,doc):
+    canvas.saveState()
+    canvas.setFont('Times-Roman',9)
+    canvas.drawString(inch, 0.75 * inch, "Page %d" % doc.page)
+    canvas.restoreState()
+
+#NIVEL 2: CREAMOS LOS FLOWABLES
+#==============================
+#Creamos la hoja de Estilo
+estilo=getSampleStyleSheet()
+
+#Iniciamos el platypus story
+story=[]
+
+#Añadimos al story los flowables. Hay que tener en cuenta que se inicia
+#con el primer pageTemplate "UnaColumna"
+story.append(Paragraph("Esto es el texto del Frame normal del pagetemplate" +\
+                       " de una columna"* 500, estilo['Normal']))
+                        
+story.append(NextPageTemplate('DosColumnas'))  # Cambio de PageTemplate
+story.append(PageBreak())  # Inicio en otra hoja
+story.append(Paragraph("Esto es el texto del Frame que pertenece al" +\
+                       " pagetemplate de dos columnas" * 500, estilo['Normal']))
+                
+story.append(NextPageTemplate('UnaColumna'))
+story.append(PageBreak())
+story.append(Paragraph("Regresamos al texto del Frame normal del" +\
+                        " pagetemplate de dos columnas"*100, estilo['Normal']))
+
+#NIVEL 3: CREAMOS LOS FRAMES, para luego asignarlos a un pagetemplate.
+#===========================
+#Frame (x1, y1, ancho, alto, leftPadding=6, bottomPadding=6, rightPadding=6,
+# topPadding=6, id=None, showBoundary=0)
+
+#1. Frame que contendrá a toda el contenido de una hoja
+frameN = Frame(inch, inch, 451, 697, id='normal')
+
+#2. Frame de columnas
+frame1 = Frame(inch, inch, 220, 697, id='col1')
+frame2 = Frame(inch + 230, inch, 220, 697, id='col2')
+
+#NIVEL 4: CREAMOS LOS PAGETEMPLATE, le asignamos los frames y los canvas
+#=================================
+#PageTemplate(id=None,frames=[],onPage=_doNothing,onPageEnd=_doNothing)
+PTUnaColumna = PageTemplate(id='UnaColumna', frames=frameN, onPage=pie)
+PTDosColumnas =  PageTemplate(id='DosColumnas', frames=[frame1,frame2],
+                        onPage=encabezado, onPageEnd=pie)
+
+#NIVEL 5: CREAMOS EL DOCTEMPLATE, a partir del BaseDocTemplate
+#===============================
+doc = BaseDocTemplate('test.pdf', pageTemplates=[PTUnaColumna, PTDosColumnas], 
+        pagesize=A4)
+
+#Construimos el PDF
+doc.build(story)
+
+os.system("test.pdf")
 
 
 from django.contrib.auth.backends import ModelBackend
