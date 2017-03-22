a361f6247f5f7377b1f18f9bc44d78eed7296581
demaro <demaromail@gmail.com>
2017-03-22 19:47:40 -0300

    pdf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -441,37 +441,6 @@ class ReportePedidosPDF(View):
               #Definimos la coordenada donde se dibujará la tabla
               detalle_orden.drawOn(pdf, 440, 0)
 
-    def split(table, table_style, table_align, availableSpace,tablas = []):
-
-        tb = Table(table)
-        tb.setStyle(table_style)
-        tb.hAlign = table_align
-        if
-          tb.minWidth() <= availableSpace:
-     
-          tablas.append(tb)
-
-        else:
-          indexes = range(len(table[0]))
-          indexes.sort(reverse = True)
-        for x in indexes:
-            pos = x
-            newTable = [
-            y[0:x] for y in table]
-          
-            tb = Table(newTable)
-            tb.setStyle(table_style)
-            tb.hAlign =
-            table_align
-            if
-            tb.minWidth() <= availableSpace:
-                  
-            tablas.append(tb)
-                   break 
-            splitNew = [y[pos:] for y in table]
-            split(splitNew, table_style, table_align,
-            availableSpace, tablas)
-
     def get(self, request, id_especialidad, *args, **kwargs):
         #Indicamos el tipo de contenido a devolver, en este caso un pdf
         response = HttpResponse(content_type='application/pdf')
