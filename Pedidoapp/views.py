9c0e97d544a81741925e08a128a7b89bf9f2f456
demaro <demaromail@gmail.com>
2017-03-22 20:32:57 -0300

    pdf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -430,7 +430,7 @@ class ReportePedidosPDF(View):
               detalle_orden.drawOn(pdf, 800, 2200)
         elif count >50 and count <=70:
               #Definimos la coordenada donde se dibujará la tabla
-              detalle_orden.drawOn(pdf, 800, 1900)
+              detalle_orden.drawOn(pdf, 800, 1950)
         elif count >70 and count <=90:
               #Definimos la coordenada donde se dibujará la tabla
               detalle_orden.drawOn(pdf, 440, 580)
