dd3902453aeee46e2d0bc14a721aeeb5d1a2cc6d
demaro <demaromail@gmail.com>
2017-03-22 17:28:21 -0300

    pdf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -395,9 +395,9 @@ class ReportePedidosPDF(View):
         pdf.setFont("Helvetica", 14)
         pdf.drawString(330, 1100, u"PEDIDO POR ESPECIALIDAD")
         pdf.setFont("Helvetica", 10)
-        pdf.drawString(625, 1000, u"FECHA: " + str(datetime.date.today()))
+        pdf.drawString(625, 1050, u"FECHA: " + str(datetime.date.today()))
         pdf.setFont("Helvetica", 10)
-        pdf.drawString(100, 1000, u"Solicitado por: " + str(especialidad.encargado.nombre))
+        pdf.drawString(100, 1050, u"Solicitado por: " + str(especialidad.encargado.nombre))
 
 
     def tabla(self,pdf,y, id_especialidad):
