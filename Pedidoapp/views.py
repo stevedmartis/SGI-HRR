c1e922730d1cebe9800eea21925c32e1e970fec9
demaro <demaromail@gmail.com>
2017-03-22 16:40:24 -0300

    utf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -421,7 +421,7 @@ class ReportePedidosPDF(View):
             ]
         ))
         #Establecemos el tamaño de la hoja que ocupará la tabla 
-        detalle_orden.wrapOn(pdf, 1000, 1500)
+        detalle_orden.wrapOn(pdf, 1000, 500)
         #Definimos la coordenada donde se dibujará la tabla
         detalle_orden.drawOn(pdf, 100, 400)
 
