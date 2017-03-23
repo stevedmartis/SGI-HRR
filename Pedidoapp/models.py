3a54615ed23d9cf62ae3d900ce82a90621ad6e46
demaro <demaromail@gmail.com>
2017-03-22 23:24:32 -0300

    pdf

--- Pedidoapp/models.py
+++ Pedidoapp/models.py
@@ -40,7 +40,7 @@ class Pedido(models.Model):
 
 
 def update_total(sender, instance, **kwargs):
-    instance.articulo.total_pedido += instance.cantidad -1
+    instance.articulo.total_pedido += instance.cantidad
     instance.articulo.save()
 
 # register the signal
