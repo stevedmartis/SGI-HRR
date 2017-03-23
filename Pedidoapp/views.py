ca4f1d44c9a3dc7dff023667f1b7890ba13a6123
demaro <demaromail@gmail.com>
2017-03-22 21:46:58 -0300

    pdf

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -445,7 +445,7 @@ class ReportePedidosPDF(View):
               detalle_orden.drawOn(pdf, 800, 600)
         elif count >150 and count <=170:
               #Definimos la coordenada donde se dibujará la tabla
-              detalle_orden.drawOn(pdf, 800, 500)
+              detalle_orden.drawOn(pdf, 800, 400)
 
     def get(self, request, id_especialidad, *args, **kwargs):
         #Indicamos el tipo de contenido a devolver, en este caso un pdf
