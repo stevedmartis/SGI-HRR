edd008257e8111b04555dae95a28b459eb9a400d
demaro <demaromail@gmail.com>
2017-03-23 10:34:57 -0300

    signals

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -504,7 +504,7 @@ class ReporteTotalPDF(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 100, 100)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 800, 2640)
+        detalle_orden.drawOn(pdf, 800, 0)
         
     def get(self, request, *args, **kwargs):
         #Indicamos el tipo de contenido a devolver, en este caso un pdf
