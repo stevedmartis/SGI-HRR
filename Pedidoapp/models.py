26ec884a2215e1e5a14f3ec38a21423ec22a16f5
demaro <demaromail@gmail.com>
2017-03-23 09:20:57 -0300

    signals

--- Pedidoapp/models.py
+++ Pedidoapp/models.py
@@ -38,13 +38,18 @@ class Pedido(models.Model):
     def __str__(self):
         return '{}'.format(self.especialidad, self.articulo, self.cantidad, self.estado)
 
-
+"""
 def update_total(sender, instance, **kwargs):
     instance.articulo.total_pedido += instance.cantidad
     instance.articulo.save()
 
 # register the signal
 signals.post_save.connect(update_total, sender=Pedido, dispatch_uid="path.to.this.module")
+"""
+def save(self, *args, **kwargs):
+        self.articulo.total_pedido += self.cantidad
+        self.articulo.save()
+        super(Pedido, self).save(*args, **kwargs)
 
 class Articulo(models.Model):
     cod_experto = models.CharField(max_length=999, primary_key=True, blank=True)
