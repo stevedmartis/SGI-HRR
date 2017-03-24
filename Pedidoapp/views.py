fbdbd7617f89f4d0df3bbfd30fd3185776f9d995
demaro <demaromail@gmail.com>
2017-03-23 23:57:29 -0300

    signals

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -510,7 +510,7 @@ class ReporteTotalPDF(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 100, 100)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 30, 10)
+        detalle_orden.drawOn(pdf, 500, 10)
 
     #SEGUNDA TABLA. INSUMO  
     def tabla2(self,pdf,y):
@@ -534,7 +534,7 @@ class ReporteTotalPDF(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 100, 100)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 580, 10)
+        detalle_orden.drawOn(pdf, 900, 10)
 
     #TERCERA TABLA.INSUMO   
     def tabla3(self,pdf,y):
@@ -558,7 +558,7 @@ class ReporteTotalPDF(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 100, 100)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 1130, 10)
+        detalle_orden.drawOn(pdf, 1400, 10)
 
     def get(self, request, *args, **kwargs):
         #Indicamos el tipo de contenido a devolver, en este caso un pdf
