ceb22db5dac09530ab11154869ef86f04435f53f
demaro <demaromail@gmail.com>
2017-03-23 23:27:45 -0300

    signals

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -541,7 +541,7 @@ class ReporteTotalPDF(View):
         #Creamos una tupla de encabezados para neustra tabla
         encabezados = ('Codigo Experto', 'Nombre Articulo', 'Stock', 'Bodega', 'Total Pedido')
         #Creamos una lista de tuplas que van a contener a las personas
-        detalles = [(art.cod_experto, art.nombre, art.stock, art.info_bodega, art.total_pedido) for art in Articulo.objects.filter(cod_experto__range=["DRE-0086", "VV-0122"]).order_by('cod_experto')]
+        detalles = [(art.cod_experto, art.nombre, art.stock, art.info_bodega, art.total_pedido) for art in Articulo.objects.filter(cod_experto__range=["DRE-0086", "VV-0122"]).filter(info_bodega=1).order_by('cod_experto')]
         #Establecemos el tamaño de cada una de las columnas de la tabla
         detalle_orden = Table([encabezados] + detalles, colWidths=[3 * cm, 10 * cm, 2 * cm, 2 * cm, 2 * cm])
         #Aplicamos estilos a las celdas de la tabla
