9d4fe3fd59c2fb95a14b39c01a7a332e82dfd0b2
demaro <demaromail@gmail.com>
2017-03-24 09:06:37 -0300

    signals

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -587,14 +587,14 @@ class ReporteTotalFarmacia(View):
 
     def cabecera(self,pdf ):
         #Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
-        pdf.setFont("Helvetica", 24)
-        #Dibujamos una cadena en la ubicación X,Y especificada
-        pdf.drawString(500, 1500, u"REPORTE ARTICULOS CAE")
         pdf.setFont("Helvetica", 22)
+        #Dibujamos una cadena en la ubicación X,Y especificada
+        pdf.drawString(450, 1500, u"REPORTE ARTICULOS CAE")
+        pdf.setFont("Helvetica", 20)
         pdf.drawString(400, 1950, u"TOTAL DE CANTIDADES SOLICITADAS")
-        pdf.setFont("Helvetica", 20)
+        pdf.setFont("Helvetica", 18)
         pdf.drawString(200, 2100, u"FECHA: " + str(datetime.date.today()))
-        pdf.setFont("Helvetica", 20)
+        pdf.setFont("Helvetica", 18)
         pdf.drawString(800, 2100, u"DE BODEGA: FARMACIA")
    
     def tabla(self,pdf,y):
