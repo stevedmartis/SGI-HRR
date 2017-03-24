271d0c1be9583bead2b422867ba99af6e8cb9bd2
demaro <demaromail@gmail.com>
2017-03-23 22:45:37 -0300

    signals

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -510,7 +510,7 @@ class ReporteTotalPDF(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 100, 100)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 30, 400)
+        detalle_orden.drawOn(pdf, 30, 200)
     #SEGUNDA TABLA.    
     def tabla2(self,pdf,y):
         #Creamos una tupla de encabezados para neustra tabla
