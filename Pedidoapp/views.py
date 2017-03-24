f569e8527c41f2560900ee400f025a1a14b30451
demaro <demaromail@gmail.com>
2017-03-24 08:26:09 -0300

    signals

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -647,13 +647,13 @@ class ReporteTotalEcono(View):
         #Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
         pdf.setFont("Helvetica", 26)
         #Dibujamos una cadena en la ubicación X,Y especificada
-        pdf.drawString(1000, 2300, u"REPORTE ARTICULOS CAE")
+        pdf.drawString(600, 2300, u"REPORTE ARTICULOS CAE")
         pdf.setFont("Helvetica", 24)
-        pdf.drawString(1000, 2250, u"TOTAL DE CANTIDADES SOLICITADAS")
+        pdf.drawString(500, 2250, u"TOTAL DE CANTIDADES SOLICITADAS")
         pdf.setFont("Helvetica", 22)
-        pdf.drawString(1500, 2200, u"FECHA: " + str(datetime.date.today()))
+        pdf.drawString(800, 2200, u"FECHA: " + str(datetime.date.today()))
         pdf.setFont("Helvetica", 22)
-        pdf.drawString(800, 2200, u"DE BODEGA: ECONOMATO")
+        pdf.drawString(400, 1200, u"DE BODEGA: ECONOMATO")
    
     def tabla(self,pdf,y):
         #Creamos una tupla de encabezados para neustra tabla
