f22014e7619cb0721423aa531871b915c5378626
demaro <demaromail@gmail.com>
2017-03-30 00:58:35 -0300

    beta

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -464,7 +464,6 @@ def DeletePedido(request, id_pedido, id_especialidad, cod_experto):
   pedido       = Pedido.objects.get(id=id_pedido)
   if request.method == 'GET':
     pedido.delete()
-    return HttpResponseRedirect('/solicitar/lista_super/%s/' % id_especialidad)
   return render(request, 'delete.html', {'especialidad':especialidad, 'articulo':articulo, 'pedido':pedido})
 
 
