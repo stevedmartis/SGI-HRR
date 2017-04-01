bf0f62b3d8a869cd9c236507d4ae38588d253c71
demaro <demaromail@gmail.com>
2017-03-31 23:01:06 -0300

    beta

--- Pedidoapp/forms.py
+++ Pedidoapp/forms.py
@@ -4,19 +4,30 @@ from .models import Pedido, Especialidad, Articulo, Pedido_Extra
 
 
 class PedidoEditForm(forms.ModelForm):
-    cantidad       = forms.IntegerField(label='Cantidad:', widget=forms.TextInput(attrs={'size':'10'}))
+    cantidad       = forms.IntegerField(label='Cantidad:', widget=forms.TextInput(attrs={'type':'number', 'size':'10'}))
 
     class Meta:
         model = Pedido
 
 
+        fields = [
+
+        'cantidad',
+
+        ]  
 
 class PedAdminEditForm(forms.ModelForm):
-    cantidad_update       = forms.IntegerField(label='Cantidad:', widget=forms.TextInput(attrs={'size':'10'}))
+    cantidad_update       = forms.IntegerField(label='Cantidad:', widget=forms.TextInput(attrs={'type':'number', 'size':'10'}))
 
     class Meta:
         model = Pedido
 
+
+        fields = [
+
+        'cantidad_update',
+
+        ] 
 
 class EstadisticaForm(forms.ModelForm):
     estadistica    = forms.IntegerField(label='Estadistica Mensual:', widget=forms.TextInput(attrs={'placeholder':'Ingrese numero pacientes'}))  
