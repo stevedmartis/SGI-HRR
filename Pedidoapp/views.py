4049c45e84260ed3a4479fd5968d4d1a32252ddf
demaro <demaromail@gmail.com>
2017-04-02 20:37:15 -0300

    pdf_update

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -494,13 +494,13 @@ class ReportePedidosPDF(View):
         #Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
         pdf.setFont("Helvetica", 26)
         #Dibujamos una cadena en la ubicación X,Y especificada
-        pdf.drawString(1100, 3300, u"REPORTE CAE")
+        pdf.drawString(150, 1900, u"REPORTE CAE")
         pdf.setFont("Helvetica", 24)
-        pdf.drawString(1050, 3250, u"PEDIDO POR ESPECIALIDAD")
+        pdf.drawString(130, 1850, u"PEDIDO POR ESPECIALIDAD")
         pdf.setFont("Helvetica", 22)
-        pdf.drawString(1390, 3200, u"FECHA: " + str(datetime.date.today()))
+        pdf.drawString(200, 1800, u"FECHA: " + str(datetime.date.today()))
         pdf.setFont("Helvetica", 22)
-        pdf.drawString(700, 3200, u"Solicitado por: " + str(especialidad.encargado.nombre))
+        pdf.drawString(50, 1800, u"Solicitado por: " + str(especialidad.encargado.nombre))
 
 
     def tabla(self,pdf,y, id_especialidad):
