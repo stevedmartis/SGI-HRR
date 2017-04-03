a4883d5cfc0beafad2ea348b72ec7a136142dfa7
demaro <demaromail@gmail.com>
2017-04-03 19:50:54 -0300

    pdf_update

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -421,10 +421,8 @@ def Acces_close(request):
 
 def Acces_open(request):
   acceso = Especialidad.objects.all().update(acceso=1)
-  especialidad = Especialidad.objects.all()
+  especialidad = Especialidad.objects.all().update(estado="completado")
   pedido = Pedido.objects.all().update(cantidad=0, estado="", fecha_pedido=None, fecha_entrega=None)
-  especialidad.estado = "completado"
-  especialidad.save()
   return HttpResponseRedirect('/solicitar/home/')
 
 @cache_page(3000)
