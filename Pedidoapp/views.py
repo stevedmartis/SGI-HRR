74314f4d09ee08af3ceee4871e8f70715a8b4dd7
demaro <demaromail@gmail.com>
2017-04-03 21:36:29 -0300

    pdf_update

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -338,10 +338,10 @@ def Update_stock(request, id_pedido, cod_experto, id_especialidad):
     especialidad = Especialidad.objects.get(id=id_especialidad)
     pedido = Pedido.objects.get(id=id_pedido)
     articulo = Articulo.objects.get(pk=cod_experto)
-    if articulo.cantidad_update > 0:
+    if pedido.cantidad_update > 0:
         articulo.stock -= pedido.cantidad_update
     else:
-        articulo.stock -= pedido.cantidad_update
+        articulo.stock -= pedido.cantidad
     pedido.estado = 'entregado'
     pedido.fecha_entrega = datetime.date.today()
     especialidad.estado = 'entregado'
