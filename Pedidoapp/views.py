5c644d318b671237ad48cdb2f3c10e831f7c651f
demaro <demaromail@gmail.com>
2017-04-06 12:16:26 -0300

    utf-8

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -704,7 +704,7 @@ class ReporteTotalFarmacia(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 1000, 1000)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 300, 900)
+        detalle_orden.drawOn(pdf, 300, 1200)
 
     def get(self, request, *args, **kwargs):
         #Indicamos el tipo de contenido a devolver, en este caso un pdf
