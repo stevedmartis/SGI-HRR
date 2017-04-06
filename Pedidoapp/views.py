005d5864180c5aea2761e58db80c6585759d975c
demaro <demaromail@gmail.com>
2017-04-06 12:09:03 -0300

    utf-8

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -762,7 +762,7 @@ class ReporteTotalEcono(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 1000, 1000)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 50, 200)
+        detalle_orden.drawOn(pdf, 50, 500)
 
     def tabla2(self,pdf,y):
         #Creamos una tupla de encabezados para neustra tabla
@@ -785,7 +785,7 @@ class ReporteTotalEcono(View):
         #Establecemos el tamaño de la hoja que ocupará la tabla 
         detalle_orden.wrapOn(pdf, 1000, 1000)
         #Definimos la coordenada donde se dibujará la tabla
-        detalle_orden.drawOn(pdf, 600, 200)
+        detalle_orden.drawOn(pdf, 600, 500)
 
     def get(self, request, *args, **kwargs):
         #Indicamos el tipo de contenido a devolver, en este caso un pdf
