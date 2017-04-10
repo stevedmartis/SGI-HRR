9b685fceeb4c7b26241093a0a3d1973bf1c58c2f
demaro <demaromail@gmail.com>
2017-04-10 00:43:36 -0300

    la media caga

--- Pedidoapp/views.py
+++ Pedidoapp/views.py
@@ -356,7 +356,7 @@ def Update_stock(request, id_pedido, cod_experto, id_especialidad):
 def Entregar(request, id_especialidad):
   if request.method == 'GET':
     especialidad = Especialidad.objects.get(id=id_especialidad)
-    pedido = Pedido.filter(especialidad=especialidad).filter(estado='pendiente')
+    pedido = Pedido.objects.filter(especialidad=especialidad).filter(estado='pendiente')
     if pedido.cantidad_update > 0:
         pedido.articulo.stock -= pedido.cantidad_update
     else:
