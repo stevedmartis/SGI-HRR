16663cb4a8d68ee18cdd9d11bf7ea6355ac9c606
demaro <demaromail@gmail.com>
2017-04-14 17:19:05 -0300

    la media caga

--- Pedidoapp/urls.py
+++ Pedidoapp/urls.py
@@ -12,8 +12,8 @@ admin.autodiscover()
 
 urlpatterns = [
 
-url(r'^home/$', home, name="home"),
-url(r'^check/$', check_login, name="check_login"),
+url(r'^home/$', login_required(home), name="home"),
+url(r'^check/$', login_required(check_login), name="check_login"),
 
 
 url(r'^lista_super/(?P<id_especialidad>\d+)/$', login_required(ListAll), name='lita_todo'),
